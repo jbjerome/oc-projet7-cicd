696fa310c3eabd16f2a6b90cb69356b2a07bd1a6
jbjerome <jean-baptiste@iweez.fr>
2026-09-16 10:27:31 +0200

    front: karma lcov reporter

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -27,7 +27,11 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/microcrm"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [
+        { type: "html" },
+        { type: "text-summary" },
+        { type: "lcovonly", file: "lcov.info" },
+      ],
     },
     reporters: ["progress", "kjhtml"],
     browsers: ["ChromeHeadlessNoSandbox", "ChromeHeadless", "Chrome"],
